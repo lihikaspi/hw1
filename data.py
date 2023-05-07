0785ae41b922e82a9f9be8ff62abb68d55d14412
lihikapsi <lihi.kaspi@campus.technion.ac.il>
2023-05-07 22:50:46 +0300

    done!!! pt.3

--- data.py
+++ data.py
@@ -1,8 +1,4 @@
-import sys
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import csv
 
 # season, is_holiday, hum, t1, cnt
 def load_data(path, features):
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import sys
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
 import data
 import statistics
