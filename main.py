3ed6fa22a0f5831c2f1e8b7d4c2f352b4d463ef3
lihikapsi <lihi.kaspi@campus.technion.ac.il>
2023-05-07 22:48:50 +0300

    done!!!

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import data
 import statistics
 
+
 def print_stats(dt, subtitle, features, path, is_all, which_feature):
     print(subtitle + ":")
     statistic_functions = [statistics.calc_mean, statistics.calc_stdv]
@@ -18,8 +19,9 @@ def print_stats(dt, subtitle, features, path, is_all, which_feature):
     data.print_details(df, features[2:], statistic_functions)
     data.print_joint_details(df, features[3:], statistic_functions_joint, "Cov(t1, cnt)")
 
+
 def main(argv):
-    path = argv[1];
+    path = argv[1]
     features = ["season", "is_holiday", "hum", "t1", "cnt"]
     dt = data.load_data(path, features)
     print("Question 1:")
@@ -32,14 +34,14 @@ def main(argv):
     winter_only = data.filter_by_feature(dt, "season", {3})
     holiday = data.filter_by_feature(winter_only[0], "is_holiday", {0})
     func = [statistics.calc_mean, statistics.calc_stdv]
+    label = ["Winter Holiday records:", "Winter Weekday records:"]
     print("If t1<=13.0, then:")
-    statistics.population_statistics("Winter Holiday records:", holiday[1], "t1", "cnt", 13.0, 0, func)
-    statistics.population_statistics("Winter Weekday records:", holiday[0], "t1", "cnt", 13.0, 0, func)
+    statistics.population_statistics(label[0], holiday[1], "t1", "cnt", 13.0, 0, func)
+    statistics.population_statistics(label[1], holiday[0], "t1", "cnt", 13.0, 0, func)
     print("If t1>13.0, then:")
-    statistics.population_statistics("Winter Holiday records:", holiday[1], "t1", "cnt", 13.0, 1, func)
-    statistics.population_statistics("Winter Weekday records:", holiday[0], "t1", "cnt", 13.0, 1, func)
+    statistics.population_statistics(label[0], holiday[1], "t1", "cnt", 13.0, 1, func)
+    statistics.population_statistics(label[1], holiday[0], "t1", "cnt", 13.0, 1, func)
     # double code --> fix
-
 
 
 if __name__ == '__main__':
